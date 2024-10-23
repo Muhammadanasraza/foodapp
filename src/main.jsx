ebeaaec41c3fd7e9a5cea5f3a90e68d34ceb9593
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2024-10-23 22:34:17 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import StoreContextProvider from './Context/Context.jsx'
-import AuthContextProvider from './authContext/authContext.jsx'
+import AuthContextProvider from "./AuthContext/AuthContext.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
