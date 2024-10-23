0a1593b53e38d0a5f5845a336edd2fe9db2b52cc
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2024-10-23 23:44:06 +0500

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export default function Footer() {
 
@@ -120,7 +121,7 @@ export default function Footer() {
                             </a>
                         </p>
                         <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                            <a className="text-gray-500">
+                            <Link to={"https://web.facebook.com/muhammadanasraza786"} className="text-gray-500">
                                 <svg
                                     fill="currentColor"
                                     strokeLinecap="round"
@@ -131,7 +132,7 @@ export default function Footer() {
                                 >
                                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
                                 </svg>
-                            </a>
+                            </Link>
                             <a className="ml-3 text-gray-500">
                                 <svg
                                     fill="currentColor"
@@ -144,21 +145,7 @@ export default function Footer() {
                                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
                                 </svg>
                             </a>
-                            <a className="ml-3 text-gray-500">
-                                <svg
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    className="w-5 h-5"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
-                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
-                                </svg>
-                            </a>
-                            <a className="ml-3 text-gray-500">
+                            <Link to={"https://www.linkedin.com/in/muhammad-anas-raza-b2b495289/"} className="ml-3 text-gray-500">
                                 <svg
                                     fill="currentColor"
                                     stroke="currentColor"
@@ -174,7 +161,21 @@ export default function Footer() {
                                     />
                                     <circle cx={4} cy={4} r={2} stroke="none" />
                                 </svg>
-                            </a>
+                            </Link>
+                            <Link to={"https://www.instagram.com/muhammadanasraza_/"} className="ml-3 text-gray-500">
+                                <svg
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    className="w-5 h-5"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
+                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                                </svg>
+                            </Link>
                         </span>
                     </div>
                 </div>
