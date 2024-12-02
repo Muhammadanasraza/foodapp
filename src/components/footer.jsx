1d170dadd5cfb749cc5018dc755cdeb39dee7711
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2024-12-02 19:46:17 +0500

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,9 +6,9 @@ export default function Footer() {
 
 
         <>
-            <footer className="text-gray-600 w-full body-font bg-slate-400 container flex flex-col  ">
-                <div className="container  py-24 mx-auto flex md:items-center   lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-                    <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+            <footer className="text-gray-600 w-full body-font bg-slate-400 justify-center flex flex-col  ">
+                <div className=" py-24 mx-auto flex    lg:items-center md:flex-row md:flex-nowrap flex-wrap flex-col">
+                    <div className="w-64 flex-shrink-0  mx-auto text-center md:text-left">
                         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
